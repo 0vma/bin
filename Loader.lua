4d90fffc7c529690d2b8ed98eddc7f4c9556c909
vma <106908003+0vma@users.noreply.github.com>
2022-10-27 10:38:32 +0200

    [+] - Loader, Updated AntiDump methods

--- Loader.lua
+++ Loader.lua
@@ -1,12 +1,15 @@
 if print == loadstring then game:shutdown(); -- common anti dump check (doubt anyone would actually use this LOL)
 local methods = {print, warn, error, rconsoleprint, rconsolewarn, rconsoleerr, rconsoleinfo}
+local cases = {"loadstring", "github", "www", "https://", "HTTP", "Logged"}
 for i = 1, #methods do
     if typeof(methods[i]) == 'function' then
         old = hookfunction(methods[i], function(...) -- anti http logger (multiple methods)
-              pass = ...
-              if pass:match('loadstring') or pass:match('github') then
-                  pass = 'well you tried atleast'
-                  return pass
+              local pass = ...
+              for met = 1, #cases do 
+                   if pass:match(cases[met]) then 
+                        pass = "http logging in 2022 damn bro"
+                        return pass
+                   end
               end
               return ...
         end)
@@ -21,12 +24,4 @@ if table.find(Games, gid) then
     local scs, err = pcall(function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/0vma/bin/main/"..gid, true))()
     end)
-    if not scs and err then 
-        if syn then
-            rconsoleprint('@@RED@@')
-            rconsoleprint('script failed to load, wait until it gets fixed, ERROR CODE #001 - invalid loadstring link')
-        else
-             print('script failed to load, wait until it gets fixed, ERROR CODE #001 - invalid loadstring link')
-        end
-    end
 end
